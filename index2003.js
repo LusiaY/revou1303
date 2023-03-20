149deaad1926452d0f4a39eabcefed5ba8958bfb
Lusia Y <lusiayeseniae13@gmail.com>
2023-03-20 21:02:15 +0700

    menambah javascript

---
+++ index2003.js
@@ -0,0 +1,15 @@
+// javascript
+for (let i = 0; i , 10; i++){
+    console.log("ulangan "+ i);
+}
+
+// while
+var ulangi = confirm("apakah mau ulangi?");
+var counter = 0;
+
+while(ulangi){
+    counter**;
+    ulangi = confirm("apakah mau ulangi?");
+}
+
+console.log("counter "+ counter);
